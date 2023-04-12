5c90e05f508702511d084eac79d3908678c65170
tomwilson89 <tfuenteswilson@gmail.com>
2023-04-12 11:09:29 -0400

    fix: add eslint rule to fix build error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
-import viteLogo from '../../../../../../../vite.svg';
 import './App.css';
 import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
 
 const App: React.FC = () => {
     const [count, setCount] = useState(0);
