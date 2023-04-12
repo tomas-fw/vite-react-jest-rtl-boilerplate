fb8335be1b2dced5fc7f42720d4b455755dd5d08
tomwilson89 <tfuenteswilson@gmail.com>
2023-04-12 09:56:17 -0400

    chote: configure eslint with prettier

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,10 +1,10 @@
-import { render, screen } from "@testing-library/react";
-import App from "../src/App";
+import { render, screen } from '@testing-library/react';
+import App from '../src/App';
 
-test("should page", () => {
-  render(<App />);
-  const headerElement = screen.getByRole("heading", {
-    name: /vite \+ react/i,
-  });
-  expect(headerElement).toBeInTheDocument();
+test('should page', () => {
+    render(<App />);
+    const headerElement = screen.getByRole('heading', {
+        name: /vite \+ react/i,
+    });
+    expect(headerElement).toBeInTheDocument();
 });
